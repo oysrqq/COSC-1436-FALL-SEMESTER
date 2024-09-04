e5139cf6124bf3760d9f813f3dfcf4513bf6dc22
oysrqq <izeyah.ikey@gmail.com>
2024-09-04 00:32:36 +0000

    Pending changes exported from your codespace

---
+++ BudgetCalculatorProject.java
@@ -0,0 +1,18 @@
+public class BudgetCalculatorProject
+{
+public static void main(String [] args)
+{
+    final double SALARY = 3000.0;
+    System.out.println(SALARY);
+    final double SAVINGS_PERCENT = 0.20;
+    final double RENT_PERCENT = 0.30;
+    final double GROCERIES_PERCENT = 0.15;
+    final double ENTERTAINMENT_PERCENT = 0.10;
+    System.out.println(SAVINGS_PERCENT);
+    System.out.println(RENT_PERCENT);
+    System.out.println(GROCERIES_PERCENT);
+    System.out.println(ENTERTAINMENT_PERCENT);
+    
+    
+}
+}
